6e5c08c2aa84724bb7a7825b45b260e0a45df823
davieu <davisumana@gmail.com>
2019-03-13 01:18:05 -0400

    asynchronous the old way settimeout()

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+/********************************************************
+ * 120 asynchronous js
+ */
+
+ /*
 //synchronous all the instructions are printed out in order
 const second = () => {
 //is a function that we can use a callback and also a time
@@ -13,6 +18,35 @@ const first = () => {
   console.log('The end')
 }
 
-
-
 first();
+*/
+
+
+/*********************************************************
+ * 122 asynchronous js the old way
+ */
+
+function getRecipe() {
+  setTimeout(() => {
+    let recipeID = [523, 883, 432, 974];
+    console.log(recipeID);
+
+    setTimeout((id) => {
+      const recipe = {title: 'Fresh tomato pasta', 
+                      publisher: 'Jonas'}
+      console.log(`${id}: ${recipe.title}`)
+
+      setTimeout((publisher, food) => {
+        const recipe2 = {title: 'Italian Pizza', 
+                        publisher: 'Jonas'}
+        console.log(recipe)
+        console.log(`${publisher}`)
+        console.log(food);
+        console.log(recipe2);
+      }, 1500, recipe.publisher, recipe.title)
+    }, 1000, recipeID[2]);
+  }, 1500);
+}
+
+getRecipe();
+// console.log(recipeID);
